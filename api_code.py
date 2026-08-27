0aa27e38f3b4d715a63d74c3a72b82de928a3684
brendanmcoyne <bmcoyne@bu.edu>
2026-08-27 14:36:39 -0400

    Used correct link

--- api_code.py
+++ api_code.py
@@ -21,6 +21,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://fantasy-college-football.vercel.app",
+        "https://fantasy-college-football.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
